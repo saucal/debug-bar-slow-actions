6b0fc0c73e3d940caf95f234ad3cd1d2ab3e74f1
Matias Saggiorato <matiassaggiorato@gmail.com>
2016-10-11 00:17:21 -0300

    Time between priorities of the same filter. Adds a bit of heap, but helps figure out where's the biggest performance concern.
    Also reports how much time each priority stage takes, in order to more easily identify bottlenecks.

--- debug-bar-slow-actions.php
+++ debug-bar-slow-actions.php
@@ -23,6 +23,25 @@ class Debug_Bar_Slow_Actions {
 		// add_action( 'wp_footer', function() { print_r( $this->flow ); }, 9000 );
 	}
 
+	function time_phase($ret = null) {
+		if(empty($this->stack))
+			return $ret;
+
+		$stack = end($this->stack);
+
+		$phase = $stack->phases[$stack->curr_phase];
+		$now = microtime(true);
+		if(!isset($this->flow[ $stack->filter ]['phases'][ $phase ]))
+			$this->flow[ $stack->filter ]['phases'][ $phase ] = 0;
+
+		$this->flow[ $stack->filter ]['phases'][ $phase ] += $now - $stack->phase_time - $stack->subs;
+		$stack->phase_time = $now;
+		$stack->subs = 0;
+		$stack->curr_phase++;
+
+		return $ret;
+	}
+
 	function time_start() {
 		if ( ! isset( $this->flow[ current_filter() ] ) ) {
 			$this->flow[ current_filter() ] = array(
@@ -30,13 +49,33 @@ class Debug_Bar_Slow_Actions {
 				'stack' => array(),
 				'time' => array(),
 				'callbacks' => array(),
+				'phases' => array(),
 			);
 
 			// @todo: add support for nesting filters, see #17817
-			add_action( current_filter(), array( $this, 'time_stop' ), 9000 );
+			add_action( current_filter(), array( $this, 'time_stop' ), PHP_INT_MAX );
 		}
 
-		array_push( $this->stack, current_filter() );
+		global $wp_filter;
+		$phases = array_keys($wp_filter[current_filter()]);
+		$last = array_search(PHP_INT_MAX, $phases);
+		unset($phases[$last]);
+		sort($phases, SORT_NUMERIC);
+
+		foreach($phases as $priority) {
+			add_action( current_filter(), array( $this, 'time_phase' ), $priority );
+		}
+
+		$now = microtime(true);
+		array_push( $this->stack, new DBSA_Stack_Item(array(
+			"filter" => current_filter(),
+			"start" => $now,
+			"phase_time" => $now,
+			"phases" => $phases,
+			"curr_phase" => 0,
+			"subs" => 0
+		) ) );
+
 		$count = ++$this->flow[ current_filter() ]['count'];
 		array_push( $this->flow[ current_filter() ]['stack'], array( 'start' => microtime( true ) ) );
 	}
@@ -46,10 +85,11 @@ class Debug_Bar_Slow_Actions {
 		$time['stop'] = microtime( true );
 		array_push( $this->flow[ current_filter() ]['time'], $time );
 
-		array_pop($this->stack);
+		$stack = array_pop($this->stack);
 		if(!empty($this->stack)) {
 			$last = end($this->stack);
-			array_push( $this->flow[ $last ]['time'], array("sub" => $time["stop"] - $time["start"]) );
+			$last->subs += microtime(true) - $stack->start; 
+			array_push( $this->flow[ $last->filter ]['time'], array("sub" => $time["stop"] - $time["start"]) );
 		}
 
 		// In case this was a filter.
@@ -96,6 +136,7 @@ class Debug_Bar_Slow_Actions {
 
 		foreach ( $this->flow as $action => $data ) {
 			$total = 0;
+
 			foreach ( $data['time'] as $time ){
 				if(isset($time["sub"]))
 					$total -= $time["sub"] * 1000;
@@ -137,6 +178,10 @@ class Debug_Bar_Slow_Actions {
 					$this->flow[ $action ]['callbacks_count']++;
 				}
 			}
+
+			if($this->flow[ $action ]['callbacks_count'] === 0)
+				unset($this->flow[ $action ]);
+			
 		}
 
 		uasort( $this->flow, array( $this, 'sort_actions_by_time' ) );
@@ -162,11 +207,36 @@ class Debug_Bar_Slow_Actions {
 			$callbacks_output .= '</ol>';
 
 			$table .= '<tr>';
-			$table .= sprintf( '<td><span class="dbsa-action">%s</span> %s</td>', $action, $callbacks_output );
+			$table .= sprintf( '<td><span class="dbsa-action">%s</span></td>', $action );
 			$table .= sprintf( '<td style="text-align: right;">%d</td>', $data['callbacks_count'] );
 			$table .= sprintf( '<td style="text-align: right;">%d</td>', $data['count'] );
 			$table .= sprintf( '<td style="text-align: right;">%.2fms</td>', $data['total'] / $data['count'] );
 			$table .= sprintf( '<td style="text-align: right;">%.2fms</td>', $data['total'] );
+			$table .= '</tr>';
+
+			$times_output = '<ol class="dbsa-results">';
+
+			$total_phases_time = 0;
+
+			$prev_start = 0;
+			foreach ( $data['phases'] as $priority => $time ) {
+				$total_phases_time += $time * 1000;
+				$phase_cbs = count($data['callbacks'][$priority]);
+				$perc = ($time / ($data["total"] / 1000)) * 100;
+
+				$bar = sprintf( '<span style="left: %f%%; width: %f%%;" title="%s"></span>', $prev_start, $perc, ($time * 1000) . "ms");
+				$prev_start += $perc;
+				$times_output .= sprintf( '<li>%s %s</li>', implode("<br>", array_fill(0, $phase_cbs, "&nbsp;")), $bar );
+			}
+
+			$bar = sprintf( '<span class="other" style="left: %f%%; width: %f%%;" title="%s"></span>', $prev_start, 100 - $prev_start, ($data["total"] - $total_phases_time) . "ms");
+			$times_output .= sprintf( '<li>%s %s</li>', "&nbsp;", $bar );
+
+			$times_output .= '</ol>';
+
+			$table .= '<tr class="details">';
+			$table .= sprintf( '<td>%s</td>', $callbacks_output );
+			$table .= sprintf( '<td colspan="4">%s</td>', $times_output );
 			$table .= '</tr>';
 		}
 		$table .= '</table>';
@@ -216,10 +286,6 @@ class Debug_Bar_Slow_Actions {
 				padding-left: 50px;
 				color: #777;
 				margin-top: 10px;
-				display: none;
-			}
-			.dbsa-expanded ol.dbsa-callbacks {
-				display: block;
 			}
 			.dbsa-action:before {
 				content: '\\f140';
@@ -234,13 +300,39 @@ class Debug_Bar_Slow_Actions {
 			.dbsa-expanded .dbsa-action:before {
 				content: '\\f142';
 			}
+			#dbsa-container tr.details {
+				display: none;
+			}
+			#dbsa-container .dbsa-expanded + tr.details {
+				display: table-row;
+			}
+			.dbsa-results li {
+				position: relative;
+			}
+			.dbsa-results li span {
+				position: absolute;
+				top: 0;
+				bottom: 0;
+				left: 0;
+				background: red;
+				outline: red dotted 2px;
+			}
+			.dbsa-results li span.other {
+				background: blue;
+				outline: blue dotted 2px;
+			}
+		}
 		</style>
 EOD;
 		$output .= <<<EOD
 		<script>
 			(function($){
 				$('#dbsa-container td').on('click', function() {
-					$(this).parents('tr').first().toggleClass('dbsa-expanded');
+					var parent = $(this).parents('tr').first();
+					if(parent.hasClass("details"))
+						parent = parent.prev();
+
+					parent.toggleClass('dbsa-expanded');
 				});
 			}(jQuery));
 		</script>
@@ -250,3 +342,17 @@ EOD;
 	}
 }
 new Debug_Bar_Slow_Actions;
+
+Class DBSA_Stack_Item {
+	public $filter;
+	public $start;
+	public $phase_time;
+	public $phases;
+	public $curr_phase;
+	public $subs;
+	function __construct($args) {
+		foreach($args as $i => $v) {
+			$this->$i = $v;
+		}
+	}
+}
